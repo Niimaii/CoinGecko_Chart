b49933b350254d586c2ac93fc4b13488cbfb7b3a
Niimaii <nimaicgomes@gmail.com>
2023-03-05 17:56:56 -0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import CryptoDetail from './routes/CryptoDetail';
 import CryptoHome from './routes/CryptoHome';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path='/' element={<CryptoHome />} />
         <Route path='/coin:id' element={<CryptoDetail />} />
